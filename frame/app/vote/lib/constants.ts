8bf8b609ab194f86c9ef0e466150948d106e007a
Myk <89474773+mykcryptodev@users.noreply.github.com>
2024-03-14 19:15:08 -0400

    type for chain

--- frame/app/vote/lib/constants.ts
+++ frame/app/vote/lib/constants.ts
@@ -1,25 +1,25 @@
-import { defineChain } from 'viem';
+import { Chain } from 'viem';
 import { base } from 'viem/chains';
 
-const syndicate = defineChain({
-  id: 5101,
-  name: "Syndicate",
-  nativeCurrency: {
-    name: "Ether",
-    symbol: "ETH",
-    decimals: 18,
-  },
-  rpcUrls: {
-    default: {
-      http: ["https://rpc-frame.syndicate.io"],
-    },
-  },
-  blockExplorerUrls: {
-    default: { name: "Blockscout", url: "https://explorer-frame.syndicate.io" },
-  },
-});
+// const syndicate = defineChain({
+//   id: 5101,
+//   name: "Syndicate",
+//   nativeCurrency: {
+//     name: "Ether",
+//     symbol: "ETH",
+//     decimals: 18,
+//   },
+//   rpcUrls: {
+//     default: {
+//       http: ["https://rpc-frame.syndicate.io"],
+//     },
+//   },
+//   blockExplorerUrls: {
+//     default: { name: "Blockscout", url: "https://explorer-frame.syndicate.io" },
+//   },
+// });
 
 //export const VOTE_CONTRACT = "0x41f999597F62991dE0f43860023fA2f29A99A8A5";
 //export const VOTE_CHAIN = syndicate;
 export const VOTE_CONTRACT = "0x6e259376Bac4aB281C9d1C89a6c0149D7E8a0d42";
-export const VOTE_CHAIN = base;
+export const VOTE_CHAIN: Chain = base;
